5298c05677dc997e27cf53d2f5570e3516dc1313
LonliLucas <lukej.gilless@gmail.com>
2024-02-10 22:55:26 -0700

    edited server index file and started on models

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model, Types } = require("mongoose");
+
+const userModel = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        },
+        thoughts: [
+            {
+
+            }
+        ],
+        friends: [
+            {
+
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+userModel.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userModel);
+module.exports = User;
